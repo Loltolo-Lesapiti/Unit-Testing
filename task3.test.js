2307ede50643babf3c3b941a03953ff4ee16e8b0
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-08-24 03:07:15 +0300

    Create task 3 and test 3

--- task3.test.js
+++ task3.test.js
@@ -1,21 +1,53 @@
 const Calculator = require('./task3.js');
 
-const cal= new Calculator();
+describe('Simple calculator Operations', () => {
 
-describe("Calculator test", () => {
-    test('Addition', () => {
-        expect(cal.add(1,2)).toBe(3);
-      });
-    test('Subtraction', () => {
-        expect(cal.subtract(2,1)).toBe(1);
-      });
-    test('Division', () => {
-        expect(cal.divide(2,1)).toBe(2);
-      });
-    test('Conditional statement', () => {
-        expect(()=>{cal.divide(2,0)}).toThrow(new Error('Undefined'));
-      });
-    test('Multiplication', () => {
-        expect(cal.multiply(2,1)).toBe(2);
-      });
+  describe('Addition', () => {
+    test('Test1', () => {
+      expect(Calculator.add(2, 1)).toBe(3);
+    })
+    test('Test2', () => {
+      expect(Calculator.add(1, 1)).toBe(2);
+    })
+    test('Test3', () => {
+      expect(Calculator.add(10, 5)).toBe(15);
+    })
   });
+
+  describe('Subtraction', () => {
+    test('Test1', () => {
+      expect(Calculator.subtract(2, 1)).toBe(1);
+    })
+    test('Test2', () => {
+      expect(Calculator.subtract(1, 1)).toBe(0);
+    })
+    test('Test3', () => {
+      expect(Calculator.subtract(10, 5)).toBe(15);
+    })
+  });
+
+  describe('Multiplication', () => {
+    test('Test1', () => {
+      expect(Calculator.multiply(2, 1)).toBe(2);
+    })
+    test('Test2', () => {
+      expect(Calculator.multiply(1, 1)).toBe(1);
+    })
+    test('Test3', () => {
+      expect(Calculator.multiply(10, 5)).toBe(50);
+    })
+  });
+
+  describe('Division', () => {
+    test('Test1', () => {
+      expect(Calculator.divide(2, 1)).toBe(2);
+    })
+    test('Test2', () => {
+      expect(Calculator.divide(1, 1)).toBe(1);
+    })
+    test('Test3', () => {
+      expect(Calculator.divide(10, 5)).toBe(2);
+    })
+  });
+
+});
